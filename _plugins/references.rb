1f1fd0ea2634847f27b8c72d64f52625590a3a09
Lucius Asclepius <lucius@steelmananything.com>
2022-07-03 22:42:56 -0500

    Fix reference processing
    Signed-off-by: Lucius Asclepius <lucius@steelmananything.com>

--- _plugins/references.rb
+++ _plugins/references.rb
@@ -29,7 +29,7 @@ module Jekyll
           line = line[line.index("'")+1..].strip.chomp("'")
           if !line.index("&#013;&#013;").nil?
             last = line.rindex("&#013;&#013;")
-            line = line[last+12..-1] + "\n\n" + line[0..last]
+            line = line[last+12..-1] + "\n\n" + line[0..last-1]
             line = line.gsub("&#013;", "\n")
           end
           results << "<li id=\"reference_#{shortrefid}\">#{Rinku.auto_link(Kramdown::Document.new(line, input: 'GFM').to_html, mode=:all, link_attr=nil, skip_tags=nil)}</li>\n"
