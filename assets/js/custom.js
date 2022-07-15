16bad21a2e3bb493d3f8a3a8afe1357ce60a9201
Lucius Asclepius <lucius@steelmananything.com>
2022-07-15 13:58:11 -0500

    Support reference URL rewriting for blog copy/paste
    Signed-off-by: Lucius Asclepius <lucius@steelmananything.com>

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,27 @@
 try {
+
+  if (window.location.search.search(/[?&]anchorroot/) !== -1) {
+    var anchors = document.getElementsByTagName("a");
+    let u = new URL(window.location);
+    let urlbase = u.protocol + "//" + u.host;
+    for (var i = 0; i < anchors.length; i++) {
+      if (anchors[i].href.indexOf("anchorroot") != -1 && anchors[i].href.indexOf("#") != -1) {
+        let newurl = urlbase + anchors[i].href.substring(anchors[i].href.indexOf("anchorroot") + 11);
+        anchors[i].href = newurl;
+      }
+    }
+  }
+
+  /*
+  if (window.location.search.search(/[?&]expand/) !== -1) {
+    const allDetails = document.body.querySelectorAll('details');
+    for (let i = 0; i < allDetails.length; i++) {
+      allDetails[i].setAttribute('open', '');
+    }
+  }
+  */
+
+  /*
   // https://stackoverflow.com/a/70062967
   window.addEventListener('beforeprint', () => {
     const allDetails = document.body.querySelectorAll('details');
@@ -21,13 +44,7 @@ try {
       }
     }
   });
-
-  if (window.location.search.search(/[?&]expand/) !== -1) {
-    const allDetails = document.body.querySelectorAll('details');
-    for (let i = 0; i < allDetails.length; i++) {
-      allDetails[i].setAttribute('open', '');
-    }
-  }
+  */
 } catch (e) {
   console.log(e);
 }
