8339a808adc03327fa343d9845f1fba8f14e7e08
Lucius Asclepius <lucius@steelmananything.com>
2022-07-15 13:12:00 -0500

    Automaticlaly open references
    Signed-off-by: Lucius Asclepius <lucius@steelmananything.com>

--- _plugins/references.rb
+++ _plugins/references.rb
@@ -20,19 +20,23 @@ module Jekyll
       results = ""
       if link_lines.size > 0
         results << "<h2 id=\"references\">References</h2>\n"
-        results << "<details id=\"referencesDetails\">\n"
+        results << "<details id=\"referencesDetails\" open>\n"
         results << "<summary>#{link_lines.size} references</summary>\n"
         results << "<ol>\n"
         link_lines.each do |line|
           shortref = line[1..line.index("]")]
-          shortrefid = shortref.gsub(/[^a-zA-Z0-9]/, "")
           line = line[line.index("'")+1..].strip.chomp("'")
           if !line.index("&#013;&#013;").nil?
             last = line.rindex("&#013;&#013;")
-            line = line[last+12..-1] + "\n\n" + line[0..last-1]
+            line = line[0..last-1] + "\n\n&nbsp;\n\n" + line[last+12..-1]
             line = line.gsub("&#013;", "\n")
           end
-          results << "<li id=\"reference_#{shortrefid}\">#{Rinku.auto_link(Kramdown::Document.new(line, input: 'GFM').to_html, mode=:all, link_attr=nil, skip_tags=nil)}</li>\n"
+          shortrefid = shortref.gsub(/ /, "_")
+          # https://stackoverflow.com/a/2849800
+          # "you can use !, $, &, ', (, ), *, +, ,, ;, =, something matching %[0-9a-fA-F]{2}, something matching [a-zA-Z0-9], -, ., _, ~, :, @, /, and ?"
+          shortrefid = shortrefid.gsub(/[^a-zA-Z0-9\.,_\-\&]/, "")
+          refname = 
+          results << "<li id=\"#{shortrefid}\">#{Rinku.auto_link(Kramdown::Document.new(line, input: 'GFM').to_html, mode=:all, link_attr=nil, skip_tags=nil)}</li>\n"
         end
         results << "</ol>\n"
         results << "</details>\n"
